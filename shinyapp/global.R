45514b8fc2fed5c1fc932d6dcc0d45cb46d15b56
SK. Ng <seekiatng@gmail.com>
2020-05-02 18:34:47 -0700

    render the leaflet static.

--- shinyapp/global.R
+++ shinyapp/global.R
@@ -1,5 +1,86 @@
 library(shiny)
 library(leaflet) # to render leaflet map
 library(geojsonio) # to read geojson
+library(dplyr) # to select
+library(stringr) # to replace string
+library(scales)
+library(plyr)
 
-g_geojson <- geojson_read("data/09-WPKL-New-DM-4326.geojson", what = "sp")
+c_geojsonPath <- "data/09-WPKL-New-DM-4326.geojson"
+c_propDataPath <- "data/properties.csv"
+
+# read the geojson as "SpatialPolygonsDataFrame"
+g_geojson <- geojson_read(c_geojsonPath, what = "sp")
+class(g_geojson)
+colnames(g_geojson)
+g_areaList <- g_geojson$NAMA_DM
+class(g_areaList)
+colnames(g_areaList)
+
+# mapping the area name according to geojson
+g_area_df <- data.frame("LocationGroup" = g_areaList)
+
+# TODO : here we clean up our R. mayb extract this function to another class file
+# Import the data
+propData <- read.csv(file = c_propDataPath)
+  # look at the first six rows
+  head(propData)
+  class(propData) # it is dataframe
+  # create a new area grouping
+  propData$LocationGroup <- propData$Location 
+  # add the new column to the dataframe
+  cbind(propData, propData$LocationGroup)
+  # replace value...
+  propData$LocationGroup <- as.character(propData$LocationGroup)
+  propData$LocationGroup[propData$LocationGroup == "Cheras, Kuala Lumpur"]
+  propData$LocationGroup[propData$LocationGroup == "Cheras, Kuala Lumpur"] <-"CHERAS BARU"
+  propData$LocationGroup[propData$LocationGroup == "Kepong, Kuala Lumpur"]
+  propData$LocationGroup[propData$LocationGroup == "Kepong, Kuala Lumpur"] <-"PEKAN KEPONG"
+  propData$LocationGroup[propData$LocationGroup == "Sentul, Kuala Lumpur"]
+  propData$LocationGroup[propData$LocationGroup == "Sentul, Kuala Lumpur"] <-"SENTUL TENGAH"
+  propData$LocationGroup[propData$LocationGroup == "Jinjang, Kuala Lumpur"]
+  propData$LocationGroup[propData$LocationGroup == "Jinjang, Kuala Lumpur"] <-"JINJANG TENGAH"
+  propData$LocationGroup[propData$LocationGroup == "Gombak, Kuala Lumpur"]
+  propData$LocationGroup[propData$LocationGroup == "Gombak, Kuala Lumpur"] <-"GOMBAK SELATAN"
+  # more to map!!!
+  
+  # show the row without price
+  propData[-which(propData$Price == ""), ]
+  # remove the row without price
+  propData <- propData[!(is.na(propData$Price) | propData$Price==""), ]
+  
+  # clean up the price
+  # format the price
+  propData$Price[propData$Price == "Gombak, Kuala Lumpur"] <-"GOMBAK SELATAN"
+  class(propData$Price)
+  propData$Price <- str_replace(
+    propData$Price, # column we want to search
+    pattern = ',', # what to find
+    replacement = '' # what to replace it with
+  )
+  propData$Price <- str_replace(
+    propData$Price, # column we want to search
+    pattern = ',', # what to find
+    replacement = '' # what to replace it with
+  )
+  propData$Price <- str_replace(
+    propData$Price, # column we want to search
+    pattern = 'RM', # what to find
+    replacement = '' # what to replace it with
+  )
+  propData$Price[is.na(propData$Price)]
+  propData$Price <- as.numeric(propData$Price)
+  
+  # show the names of the dataframe
+  colnames(propData)
+# take the "Price" and "LocationGroup"
+propPriceData <- propData %>% select(-Location, -Rooms, -Bathrooms, - Size, -Furnishing, -Car.Parks, -Property.Type)
+  # check the new dataframe
+  head(propPriceData)
+  colnames(propPriceData)
+  class(propPriceData$Price) # the price is character
+# grouping the same LocationGroup
+groupedPropData <- ddply(propPriceData, .(LocationGroup), summarize,  Rate1=mean(Price))
+class(groupedPropData)
+
+areaPriceData <- left_join(g_area_df, groupedPropData, by="LocationGroup")
--- shinyapp/server.R
+++ shinyapp/server.R
@@ -1,30 +1,35 @@
 function(input, output, session) {
 
-        # create the legend bins
-    bins<-seq(from = 0, to = 100, by = 10)
+    g_max_range = 200000 # range price from 0 to 2mil
+    
+    # create the legend bins
+    bins<-seq(from = 0, to = g_max_range, by = 10)
     qpal <- colorNumeric(
         na.color = "white",
         palette = c("white", "orange", "red"),
-        domain = c(0, 100)
+        domain = c(0, g_max_range)
     )
     
     # Render map
     output$map <- renderLeaflet({
         leaflet(g_geojson) %>%
             addTiles() %>%
-            addPolygons(stroke = FALSE,
+            addPolygons(stroke = F,
                         smoothFactor = 0.3,
                         fillOpacity = 0.5,
-                        label = ,# paste(g_country_list, ":", df_pop_ratio()[,1]),
-                        color = # qpal(rescale(x = df_pop_ratio()[,1], to = c(0, 100),  na.rm=TRUE))
+                        # display by the price first...
+                        # TODO : then we move to display the psf
+                        # TODO : maybe interactive recalculate by select option prop_price or psf
+                        label = paste(g_areaList, ":", areaPriceData[,-1]),
+                        color = qpal(rescale(x = areaPriceData[,-1], to = c(0, g_max_range),  na.rm=TRUE))
             ) %>%
             setView(lat = 3.138951, lng = 101.694423, zoom = 12) %>%
             addLegend(
                 "bottomleft",
                 pal = qpal,
-                values = c(0,100),
+                values = c(0,g_max_range),
                 opacity = 0.7,
-                labFormat = labelFormat(suffix = "%"),
+                labFormat = labelFormat(suffix = "", prefix="RM"),
             )
     })
 }
